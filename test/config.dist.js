a5ba4ebc83ab771420fed0f253c5718a6692031d
Tobias Dammers <tdammers@gmail.com>
2017-04-03 18:57:09 +0200

    Support write operations to firebase DB (#4)
    * Support write operations to firebase DB
    
    * Fixed incorrect JS FFI code.
    
    Passing the path to the `.set` function is wrong, the `ref` already
    contains it.
    
    * Test project that runs Firebase.Database through its paces.

---
+++ test/config.dist.js
@@ -0,0 +1,12 @@
+// Example configuration file. Edit to taste and dump
+// in dist/config.js.
+//
+// Alternatively, the config.js generated from the firebase console
+// would also do just fine.
+config = {
+  apiKey: "YOUR API KEY HERE",
+  authDomain: "YOURAPP.firebaseapp.com",
+  databaseURL: "https://YOURAPP.firebaseio.com",
+  storageBucket: "YOURAPP.appspot.com",
+  messagingSenderId: "YOUR SENDER ID HERE"
+};
